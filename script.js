f2462450f86b6dccee565119f3fa4ab4a6cdc91c
Clinton Johnson <dragoon1246224@gmail.com>
2018-08-09 11:41:56 -0400

    Done

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ document.getElementById("countButton").onclick = function() {
         let typedText = document.getElementById("textInput").value;
         typedText = typedText.toLowerCase();
         typedText = typedText.replace(/[^a-z'\s]+/g, "");
+        // typedWords = typedWords.toLowerCase();
+        words = typedText.split(/\s/);
         letterCounts = {};
+        wordCounts = {};
         
 
     for(let i = 0; i < typedText.length; i++) {
@@ -15,7 +18,6 @@ document.getElementById("countButton").onclick = function() {
      } else {
         letterCounts[currentLetter]++;
      }
-     
      }
     
      for(let letter in letterCounts) {
@@ -24,6 +26,25 @@ document.getElementById("countButton").onclick = function() {
         span.appendChild(textContent);
         document.getElementById("lettersDiv").appendChild(span);
      }
+     
+
+     for(let x = 0; x < words.length; x++) {
+        currentWord = words[x];
     
-     words = typedText.split(/\s/)
+     if(wordCounts[currentWord] === undefined) {
+        wordCounts[currentWord] = 1;
+     } else {
+        wordCounts[currentWord]++;
+     }
+     
+     }
+
+
+     for(let word in wordCounts) {
+        let span = document.createElement("span");
+        let textContent = document.createTextNode('"' + word + "\": " + wordCounts[word] + ", ");
+        span.appendChild(textContent);
+        document.getElementById("wordsDiv").appendChild(span);
+     }
+    
  }
